b3f06e6ec9236024ae252081057ca8633442335d
spynappels <stefan.pynappels@sptechnical.co.uk>
2016-04-27 15:42:55 +0100

    class

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,19 @@
+class 'memcached' {
+
+  package { 'memcached':
+    ensure => present,
+    enable => true,
+  }
+  
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    source => 'puppet:///modules/memcached/config',
+    requires => Package['memcached'],
+  }
+  
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
+}
