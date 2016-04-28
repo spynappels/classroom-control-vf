f01443b97c5fd2cdc62b23d8dc7f65a8c1812fc1
spynappels <stefan.pynappels@sptechnical.co.uk>
2016-04-28 09:14:08 +0100

    change to notify

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -28,17 +28,18 @@ class nginx {
     ensure => present,
     source => 'puppet:///modules/nginx/nginx.conf',
     require => File['conf_dir'],
+    notify => Service['nginx'],
   }
   file {'server_block':
     path => '/etc/nginx/conf.d/default.conf',
     ensure => present,
     source => 'puppet:///modules/nginx/default.conf',
     require => File['conf_subdir'],
+    notify => Service['nginx'],
   }
   service {'nginx':
     ensure => running,
     enable => true,
-    subscribe => File['main_server'],
   }
 
 }
