92b37c70a7d78ef2428154fb490dd484641d7513
spynappels <stefan.pynappels@sptechnical.co.uk>
2016-04-28 12:52:11 +0100

    virtual notify

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,4 +42,10 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  case $::virtual {
+    'virtual'{
+      $value = capitalize($::virtual),
+      notify["This is a ${value} node"],
+    }
+  }
 }
