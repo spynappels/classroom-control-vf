549c295191bbf05e652ca82ae5d72a1709e132ad
spynappels <stefan.pynappels@sptechnical.co.uk>
2016-04-28 10:24:30 +0100

    code consolidation

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,10 @@
 class nginx {
+  File {
+    mode => '0644',
+    owner => 'root',
+    group => 'root',
+  }
+  
   package {'nginx':
     ensure => present,
   }
@@ -28,18 +34,17 @@ class nginx {
     ensure => present,
     source => 'puppet:///modules/nginx/nginx.conf',
     require => File['conf_dir'],
-    notify => Service['nginx'],
   }
   file {'server_block':
     path => '/etc/nginx/conf.d/default.conf',
     ensure => present,
     source => 'puppet:///modules/nginx/default.conf',
     require => File['conf_subdir'],
-    notify => Service['nginx'],
   }
   service {'nginx':
     ensure => running,
     enable => true,
+    subscribe => [ File['main_server'],File['server_block']],
   }
 
 }
