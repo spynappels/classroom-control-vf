c635f70a99dee445c8b03c1be9bfbfe478659390
spynappels <stefan.pynappels@sptechnical.co.uk>
2016-04-29 10:19:06 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,7 @@
 class nginx (
 $root = undef
-){
+) inherits nginx::params {
 
-case $::osfamily {
-  'redhat' : {
-    $package = 'nginx'
-    $owner = 'root'
-    $group = 'root'
-    $docroot = '/var/www'
-    $confdir = '/etc/nginx'
-    $blockdir = '/etc/nginx/conf.d'
-    $logdir = '/var/log/nginx'
-    $nginxuser = 'nginx'
-    }
-  'debian' : {
-    $package = 'nginx'
-    $owner = 'root'
-    $group = 'root'
-    $docroot = '/var/www'
-    $confdir = '/etc/nginx'
-    $blockdir = '/etc/nginx/conf.d'
-    $logdir = '/var/log/nginx'
-    $nginxuser = 'www-data'
-    }
-  'windows' : {
-    $package = 'nginx-service'
-    $owner = 'Administrator'
-    $group = 'Administrators'
-    $docroot = 'C:/ProgramData/nginx/html'
-    $confdir = 'C:/ProgramData/nginx'
-    $blockdir = 'C:/ProgramData/nginx/conf.d'
-    $logdir = 'C:/ProgramData/nginx/logs'
-    $nginxuser = 'nobody'
-    }
-  }
   if $root == undef {
     $master_docroot = $docroot
   } else {
